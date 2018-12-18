ae8dde8e12ffc771305c734109b5528f8d8b75b8
Vaishnav <vaishnavsm@gmail.com>
2018-12-19 03:21:52 +0530

    engine work

--- engine/engine.js
+++ engine/engine.js
@@ -1,20 +1,34 @@
-function showText(text){
+function showText(text, right){    
     var newBlob = document.createElement('div');
+    if(right === true) $(newBlob).addClass('right');
     $(newBlob).addClass('left');
     $(newBlob).html(text);
     $(".body").append(newBlob);
 }
 
+function end(score){
+    alert("Ended with score "+score);
+}
+
+let startTime = -1;
+
+function mainLoop(){
+    startTime = new Date().getTime();
+    $('form#user').on('submit', (e)=>{
+        e.preventDefault();
+        const inputVal = $('#input').val();
+        showText(inputVal, true);
+        $('#input').val('');
+        input(inputVal);
+    });
+}
+
 $(document).ready(()=>{
-    console.log(0)
+    
     var script = document.createElement('script');
     script.onload = ()=>{
-        alert('loaded script');
-        start().then(()=>{
-            input('hi');
-        });
+        start().then(mainLoop);
     }
     script.src = 'deploy.js';
     document.head.appendChild(script);
-    console.log(1)
 });
