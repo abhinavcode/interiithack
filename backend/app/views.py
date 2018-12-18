e5aa6152b60dc3eb77376165248aa0b0fc368100
Vaishnav <vaishnavsm@gmail.com>
2018-12-19 03:51:59 +0530

    new backend model

--- backend/app/views.py
+++ backend/app/views.py
@@ -12,15 +12,13 @@ from rest_framework.parsers import JSONParser
 def root(request):
     return HttpResponse("Hi")
 
-def get_level(user, game):
-    """
-    ADD COOL AI TO SELECT LEVEL HERE!
-    """
-    return 1
-
 def add_played_game(play):
     """
     Do cool analytics and processing and stuff here. Change the user level if needed.
+    """
+def get_recommended_games(user):
+    """
+    Add Cool AI To Select Game Here!
     """
 def get_user_analytics(user):
     return {"detail":"lol"}
@@ -33,28 +31,27 @@ class UserViewSet(viewsets.ModelViewSet):
             user = User.objects.get(pk=pk)
         except:
             return Response(json.dumps({'detail':'No such user'}), status=status.HTTP_404_NOT_FOUND)
-        return JsonResponse(get_user_analytics(user), status=400)
+        return JsonResponse(get_user_analytics(user), status=200)
+    @action(detail=True, methods=['get'])
+    def recommend(self, request, pk=None):
+        try:
+            user = User.objects.get(pk=pk)
+        except:
+            return Response(json.dumps({'detail':'No such user'}), status=status.HTTP_404_NOT_FOUND)
+        return JsonResponse(get_recommended_games(user), status=200)
 
 class GameViewSet(viewsets.ModelViewSet):
     queryset = Game.objects.all()
     serializer_class = GameSerializer
     @action(detail=True, methods=['get'])
     def play(self, request, pk=None):
-        if('user' not in request.GET):
-            return Response(json.dumps({'detail':'user not in GET parameter'}), status=status.HTTP_400_BAD_REQUEST)
         try:
             game = Game.objects.get(pk=pk)
         except:
             return Response(json.dumps({'detail':'No such game'}), status=status.HTTP_404_NOT_FOUND)
-        
-        try:
-            user = User.objects.get(pk=request.GET['user'])
-        except:
-            return Response(json.dumps({'detail':'No such user'}), status=status.HTTP_404_NOT_FOUND)
         game.plays += 1
         game.save()
-        level = get_level(user, game)
-        return JSONResponse({'code':game.code, 'level':level}, status=status.HTTP_200_OK)
+        return JSONResponse({'code':game.code}, status=status.HTTP_200_OK)
         
 class PlayViewSet(viewsets.ModelViewSet):
     queryset = Play.objects.all()
