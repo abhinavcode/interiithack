c793b72032a694096ad47347c1363930a68e307a
Vaishnav <vaishnavsm@gmail.com>
2018-12-19 16:11:25 +0530

    Heroku

--- engine/tuto.js
+++ engine/tuto.js
@@ -6,7 +6,7 @@ function proceedDemo(){
     showText("Hello!");
     showText("Let me introduce you to this game.");
     showText("Would you please press the 'A' key on your phone?");
-    showImage("http://127.0.0.1:8000/media/a.png");
+    showImage("https://techmeetbhu.herokuapp.com/media/a.png");
 }
 
 function start() {
--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,3 +1,3 @@
-const WEB_URL = 'http://127.0.0.1:8000/app/';
-const MEDIA_URL = 'http://127.0.0.1:8000';
+const WEB_URL = 'https://techmeetbhu.herokuapp.com/app/';
+const MEDIA_URL = 'https://techmeetbhu.herokuapp.com';
 const USER_UID = "9876543210";
