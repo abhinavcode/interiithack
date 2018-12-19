209858304d7259ca3e2d073901670dc7f01e64bc
Akshay Sharma <contact@imakshay.com>
2018-12-19 16:34:01 +0530

    add chatbot

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -14,6 +14,7 @@ function fetchGames() {
 	});
 }
 
+const replies = ["Sorry. Can you reply clearly", "Sorry, didn't get you","I need more training","I beg your pardon","I'm stupid. Didn't get you"];
 let _alias_requestSendMessage = requestSendMessage;
 var requestSendMessage = function() {
 	let msg = $('#chat_message').val();
@@ -22,6 +23,15 @@ var requestSendMessage = function() {
 	for (let i = 0; i < listGames.length; i++) {
 		if (msg.toLowerCase().indexOf(listGames[i].name.toLowerCase()) > -1){
 			launchGame(listGames[i].id);
+			break;
+		}else if (msg.toLowerCase().indexOf("hi") > -1 || msg.toLowerCase().indexOf("hello") > -1 ){
+			showChatText("Hi", true);
+			break;
+		}else if (msg.toLowerCase().indexOf("how") > -1 && msg.toLowerCase().indexOf("are") > -1 ){
+			showChatText("I'm fine. How are you?", true);
+			break;
+		}else{
+			showChatText(replies[msg.length%replies.length], true);
 			break;
 		}
 	}
